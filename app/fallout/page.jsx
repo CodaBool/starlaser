d5193b0407e499daa3d24a61316323ff8ce97807
Coda Bool <CodaBool@pm.me>
2025-01-06 23:07:15 -0500

    end of day8

---
+++ app/fallout/page.jsx
@@ -0,0 +1,33 @@
+// export const dynamic = 'force-static'
+import fs from "fs"
+import path from "path"
+import { feature } from 'topojson-client'
+import Cartographer from "@/components/cartographer"
+
+export default async function mapLobby({ params }) {
+  // const dataDir = path.join(process.cwd(), "/app", "[map]", "topojson");
+  // const { map } = await params
+  // const filePath = path.join(dataDir, `${map}.json`)
+  // if (map === "favicon.ico") return
+  // if (map === "lancer") return
+  // const content = await fs.promises.readFile(filePath, 'utf8')
+  // console.log("path", path.resolve(`app/[map]/topojson/fallout.json`))
+  // const content = await fs.promises.readFile(path.resolve(`app/[map]/topojson/${map}.json`), 'utf8')
+  const content = await fs.promises.readFile(process.cwd() + '/app/fallout/fallout.json', 'utf8')
+  // const content = fs.readFileSync(filePath, 'utf-8')
+  const topojson = JSON.parse(content)
+
+  // TODO: the layer name here will be different for each map
+  const layers = Object.keys(topojson.objects)
+  const layerObjects = layers.reduce((acc, layer) => {
+    acc[layer] = feature(topojson, topojson.objects[layer]).features
+    return acc
+  }, {})
+  return <Cartographer data={layerObjects} map="fallout" />
+}
+
+// export async function generateStaticParams() {
+//   const dataDir = path.join(process.cwd(), "/app", "[map]", "topojson");
+//   const folders = fs.readdirSync(dataDir).filter(f => fs.statSync(path.join(dataDir, f)))
+//   return folders.map(folder => ({ slug: folder }))
+// }
--- next.config.js
+++ next.config.js
@@ -1,15 +1,14 @@
-/** @type {import('next').NextConfig} */
-import path from "path"
+// /** @type {import('next').NextConfig} */
+// import path from "path"
 
 
-const nextConfig = {
-  experimental: {
-    nftTracing: true,
-    // outputFileTracingRoot: path.join(__dirname),
-    outputFileTracingIncludes: {
-      "/app/[map]/topojson": ["./app/[map]/topojson/**/*"]
-    }
-  }
-};
+// const nextConfig = {
+//   experimental: {
+//     // outputFileTracingRoot: path.join(__dirname),
+//     outputFileTracingIncludes: {
+//       "/app/[map]/topojson": ["./app/[map]/topojson/**/*"]
+//     }
+//   }
+// };
 
-export default nextConfig;
+// export default nextConfig;
