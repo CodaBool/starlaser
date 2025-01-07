36bc32bf47af5ab20d4c9b97b65b3b411840afd4
Coda Bool <CodaBool@pm.me>
2025-01-06 22:58:30 -0500

    end of day7

--- app/[map]/page.jsx
+++ app/[map]/page.jsx
@@ -6,13 +6,13 @@ import Cartographer from "@/components/cartographer"
 
 
 export default async function mapLobby({ params }) {
-  const dataDir = path.join(process.cwd(), "/app", "[map]", "topojson");
+  // const dataDir = path.join(process.cwd(), "/app", "[map]", "topojson");
   const { map } = await params
-  const filePath = path.join(dataDir, `${map}.json`)
+  // const filePath = path.join(dataDir, `${map}.json`)
   if (map === "favicon.ico") return
   if (map === "lancer") return
   // const content = await fs.promises.readFile(filePath, 'utf8');
-  const content = await fs.promises.readFile(path.resolve(`app/[map]/topojson/${map}`), "utf8",)
+  const content = await fs.promises.readFile(path.resolve(`app/[map]/topojson/${map}.json`), "utf8",)
   // const content = fs.readFileSync(filePath, 'utf-8')
   const topojson = JSON.parse(content)
 
