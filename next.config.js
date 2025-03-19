f264ab3328e612bbcd4a4261b3b651e85f3aac4f
Coda Bool <CodaBool@pm.me>
2025-03-18 23:59:33 -0400

    add a cors to an api route

--- next.config.js
+++ next.config.js
@@ -1,15 +1,29 @@
-// /** @type {import('next').NextConfig} */
-// import path from "path"
+/** @type {import('next').NextConfig} */
+import path from "path"
 
 
-// const nextConfig = {
-//   reactStrictMode: false,
-//   experimental: {
-//     // outputFileTracingRoot: path.join(__dirname),
-//     outputFileTracingIncludes: {
-//       "/app/[map]/topojson": ["./app/[map]/topojson/**/*"]
-//     }
-//   }
-// };
+const nextConfig = {
+  async headers() {
+    return [
+      {
+        source: "/api/v1:path*",
+        headers: [
+          { key: "Access-Control-Allow-Origin", value: "*" },
+          { key: "Access-Control-Allow-Methods", value: "GET" },
+          { key: "Access-Control-Allow-Headers", value: "Accept, Accept-Version, Content-Length, Content-Type" },
+        ]
+      }
+    ]
+  },
 
-// export default nextConfig;
+
+  reactStrictMode: false,
+  experimental: {
+    // outputFileTracingRoot: path.join(__dirname),
+    outputFileTracingIncludes: {
+      "/app/[map]/topojson": ["./app/[map]/topojson/**/*"]
+    }
+  }
+};
+
+export default nextConfig;
