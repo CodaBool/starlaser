c6ee0e07c807afadf98c870cd330c6e57f910929
Coda Bool <CodaBool@pm.me>
2025-03-19 00:03:06 -0400

    add more options

--- next.config.js
+++ next.config.js
@@ -8,22 +8,23 @@ const nextConfig = {
       {
         source: "/api/v1:path*",
         headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
           { key: "Access-Control-Allow-Origin", value: "*" },
-          { key: "Access-Control-Allow-Methods", value: "GET" },
-          { key: "Access-Control-Allow-Headers", value: "Accept, Accept-Version, Content-Length, Content-Type" },
+          { key: "Access-Control-Allow-Methods", value: "GET,DELETE,PATCH,POST,PUT" },
+          { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
         ]
       }
     ]
   },
 
 
-  reactStrictMode: false,
-  experimental: {
-    // outputFileTracingRoot: path.join(__dirname),
-    outputFileTracingIncludes: {
-      "/app/[map]/topojson": ["./app/[map]/topojson/**/*"]
-    }
-  }
+  // reactStrictMode: false,
+  // experimental: {
+  //   // outputFileTracingRoot: path.join(__dirname),
+  //   outputFileTracingIncludes: {
+  //     "/app/[map]/topojson": ["./app/[map]/topojson/**/*"]
+  //   }
+  // }
 };
 
 export default nextConfig;
